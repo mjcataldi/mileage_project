f952fc8ca8a5056234ccfffe7e67be7921ade09b
Matthew Cataldi <mcataloe@gmail.com>
2017-08-03 16:05:47 -0500

    Initialized files for mileage.  Rails is a scrap.

--- app/models/restaurant.rb
+++
@@ -1,9 +0,0 @@
-class Restaurant < ActiveRecord::Base
-  validates :owner_id, :name, :cuisine, :address, :city, :state, :zip, presence: true
-  validates :zip, numericality:true
-
-
-  belongs_to :owner, class_name: 'User'
-  has_many :reviews
-
-end
--- app/models/review.rb
+++
@@ -1,6 +0,0 @@
-class Review < ActiveRecord::Base
-  validates :reviewer, :restaurant, :body, :title, :rating, presence: true
-
-  belongs_to :restaurant
-  belongs_to :reviewer, class_name: 'User'
-end
